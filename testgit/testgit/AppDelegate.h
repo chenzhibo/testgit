a88f8977812f423392115ef3bebd86920ff21677
chenzhibo <chenzhibo@baidu.com>
2013-12-18 20:32:41 +0800

    Initial Commit

---
+++ testgit/testgit/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by chen zhibo on 12/18/13.
+//  Copyright (c) 2013 chen zhibo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ testgit/testgitTests/testgitTests.m
@@ -0,0 +1,34 @@
+//
+//  testgitTests.m
+//  testgitTests
+//
+//  Created by chen zhibo on 12/18/13.
+//  Copyright (c) 2013 chen zhibo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testgitTests : XCTestCase
+
+@end
+
+@implementation testgitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
